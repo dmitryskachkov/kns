b20d39487d06a744a25326b4400692ca8c355457
Dmitry <m33_nft@mail.ru>
2020-06-04 10:30:52 +0500

    user login page

--- sites/all/themes/la/templates/page--user--login.tpl.php
+++ sites/all/themes/la/templates/page--user--login.tpl.php
@@ -3,10 +3,18 @@
     <div class="main-panel">
       <div class="content-wrapper d-flex align-items-center auth px-0">
         <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
+          <div class="col-lg-5 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5 border">
-              <h4>Система контроля объектами</h4>
-              <h6 class="font-weight-light" style="margin-bottom: 40px">Канализационные насосные станции</h6>
+              <div class="row">
+                  <div class="col-lg-2">
+                      <i class="ti-lock" style="font-size: 40px"></i>
+                  </div>
+                  <div class="col-lg-10">
+                      <h4> Система мониторинга объектов</h4>
+                      <h6 class="font-weight-light" style="margin-bottom: 40px">Канализационные насосные станции</h6>
+
+                  </div>
+              </div>
 
               <?php print render($page['content']); ?>
 

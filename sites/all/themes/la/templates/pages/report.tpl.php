4fc053e77c96320c822482fff30ee6912940d2fb
m33_nft@mail.ru <Jq5Url4zz>
2020-07-22 09:55:34 +0500

    report

---
+++ sites/all/themes/la/templates/pages/report.tpl.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <h2>Отчеты по объекту</h2>
+                        <p class="small">Выберите объект для просмотра отчета</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <select>
+                            <option>КНС 22 Дорожный</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <fieldset>
+                            <h3>Насосы</h3>
+                            <p>Количество насосов</p>
+                            <p>Количество насосов в автоматическом режиме</p>
+                            <p>Насос №1. Время работы</p>
+                            <p>Насос №2. Время работы</p>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

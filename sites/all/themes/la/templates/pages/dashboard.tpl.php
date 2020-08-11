d54923ca881eaee79a17d938857b6f0f4291c409
m33_nft@mail.ru <Jq5Url4zz>
2020-08-11 16:07:35 +0500

    кнс 12

--- sites/all/themes/la/templates/pages/dashboard.tpl.php
+++ sites/all/themes/la/templates/pages/dashboard.tpl.php
@@ -10,7 +10,7 @@
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <a class="default-text" href="/unit/2">
-                                        <h4>КНС Дорожный</h4>
+                                        <h4>КНС 22</h4>
                                         <p class="text-muted font-weight-light">Пос. Дорожный | SE M241</p>
                                         </a>
                                     </div>
@@ -49,7 +49,58 @@
             </div>
         </div>
 
+        <div class="col-md-4 grid-margin stretch-card">
+            <div class="card">
 
+                <div class="card-body">
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <a class="default-text" href="/unit/3">
+                                        <h4>КНС 12</h4>
+                                        <p class="text-muted font-weight-light">прсп. Комсомольский | SE M241</p>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <button class="btn btn-inverse-light btn-fw" style="margin-bottom: 10px; background-color: transparent" > </button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px; " data-href="btn"  data-var="level3">6</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px; " data-href="btn" data-var="level3">5</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px; " data-href="btn"  data-var="level3">4</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px; " data-href="btn" data-var="level3">3</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px" data-href="btn"  data-var="level2">2</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px" data-href="btn"  data-var="level1">1</button>
+                                    <button class="btn btn-inverse-light btn-fw interactive" style="margin-bottom: 10px" data-href="btn" data-var="level0">0</button>
+
+                                </div>
+                                <div class="col-md-9">
+                                    <p class="small" style="margin-top:20px; margin-bottom: 20px">Насосы в работе:</p>
+                                    <div style="margin-left: 10px">
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" >1</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" >2</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" >3</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" >4</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" >5</p>
+                                    </div>
+                                    <hr>
+                                    <p style="line-height: 20px">Ошибка уровня: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn interactive" data-href="btn" data-var="unit.error_level"></button></p>
+                                    <p style="line-height: 20px">Ошибки насосов: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn  interactive" data-href="btn"data-var="unit.error_engine"></button></p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
 
        
 

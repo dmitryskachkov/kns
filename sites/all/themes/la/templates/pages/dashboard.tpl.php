a0da79370bfb1e7ede21c5f0cc5458a34d4a3237
m33_nft@mail.ru <Jq5Url4zz>
2020-10-26 09:55:26 +0500

    добавление кнс УВД

--- sites/all/themes/la/templates/pages/dashboard.tpl.php
+++ sites/all/themes/la/templates/pages/dashboard.tpl.php
@@ -57,7 +57,55 @@
 
             </div>
         </div>
+        <div class="col-md-4 grid-margin stretch-card">
+            <div class="card">
 
+                <div class="card-body">
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <a class="default-text" href="/unit/3">
+                                        <h4>КНС УВД</h4>
+                                        <p class="text-muted font-weight-light">Ул. Инженерная 5 | SE M241</p>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="bar-progress">
+                                        <span class="bar-level bar25 " data-href="btn" data-browse="" data-var="level3"> </span>
+                                        <span class="bar-level bar25 " data-href="btn" data-browse="" data-var="level2"> </span>
+                                        <span class="bar-level bar25 " data-href="btn" data-browse="" data-var="level1"> </span>
+                                        <span class="bar-level bar25 " data-href="btn" data-browse="" data-var="level0"> </span>
+
+                                    </div>
+                                </div>
+                                <div class="col-md-9">
+                                    <p class="small" style="margin-top:20px; margin-bottom: 20px">Насосы в работе:</p>
+                                    <div style="margin-left: 10px">
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" data-browse="" data-var="engine1.status">1</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" data-browse="" data-var="engine2.status">2</p>
+                                    </div>
+                                    <hr>
+                                    <p style="line-height: 20px">Режим работы: <span class="float-right interactive text-attr" data-browse="DBAVl_kns1_m241_main_mainmode" data-href="text" data-var="unit.mode">Автомат</span> </p>
+                                    <p style="line-height: 20px">Ошибка уровня: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn interactive" data-browse="" data-href="btn" data-var="unit.error_level"></button></p>
+                                    <p style="line-height: 20px">Ошибка насоса 1: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn  interactive"data-browse="" data-href="btn"data-var="unit.error_engine"></button></p>
+                                    <p style="line-height: 20px">Ошибка насоса 2: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn  interactive"data-browse="" data-href="btn" data-var="unit.error_engine"></button></p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
         <div class="col-md-4 grid-margin stretch-card">
             <div class="card">
 

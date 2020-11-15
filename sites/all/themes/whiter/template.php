d999403f140a52721124ab2b2cf2003069a6d01c
Dmitry <m33_nft@mail.ru>
2020-11-15 23:48:50 +0500

    white theme

--- sites/all/themes/whiter/template.php
+++ sites/all/themes/whiter/template.php
@@ -1,1 +1,241 @@
-/Users/dmitry/PhpstormProjects/m2m.local/sites/all/themes/la/template.php
+<?php
+
+/**
+ * Add body classes if certain regions have content.
+ */
+function whiter_preprocess_html(&$variables) {
+  if (!empty($variables['page']['featured'])) {
+    $variables['classes_array'][] = 'featured';
+  }
+
+  if (!empty($variables['page']['triptych_first'])
+    || !empty($variables['page']['triptych_middle'])
+    || !empty($variables['page']['triptych_last'])
+  ) {
+    $variables['classes_array'][] = 'triptych';
+  }
+
+  if (!empty($variables['page']['footer_firstcolumn'])
+    || !empty($variables['page']['footer_secondcolumn'])
+    || !empty($variables['page']['footer_thirdcolumn'])
+    || !empty($variables['page']['footer_fourthcolumn'])
+  ) {
+    $variables['classes_array'][] = 'footer-columns';
+  }
+  $viewport = array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+    ),
+  );
+  drupal_add_html_head($viewport, 'viewport');
+
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function whiter_process_page(&$variables) {
+
+  // Always print the site name and slogan, but if they are toggled off, we'll
+  // just hide them visually.
+  $variables['hide_site_name'] = theme_get_setting('toggle_name') ? FALSE : TRUE;
+  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
+  if ($variables['hide_site_name']) {
+    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
+    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
+  }
+  if ($variables['hide_site_slogan']) {
+    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
+    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
+  }
+  // Since the title and the shortcut link are both block level elements,
+  // positioning them next to each other is much simpler with a wrapper div.
+  if (!empty($variables['title_suffix']['add_or_remove_shortcut']) && $variables['title']) {
+    // Add a wrapper div using the title_prefix and title_suffix render elements.
+    $variables['title_prefix']['shortcut_wrapper'] = array(
+      '#markup' => '<div class="shortcut-wrapper clearfix">',
+      '#weight' => 100,
+    );
+    $variables['title_suffix']['shortcut_wrapper'] = array(
+      '#markup' => '</div>',
+      '#weight' => -99,
+    );
+    // Make sure the shortcut link is the first item in title_suffix.
+    $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
+  }
+
+  // Share urls.
+  $absolute_current_path = url(request_path(), array('absolute' => TRUE));
+  $current_page_title = urlencode(drupal_get_title());
+
+  $unit_count = db_select('eck_unit', 'u')->fields('u', ['id'])->distinct()->execute()->rowCount();
+  $tag_count = db_select('eck_tag', 't')->fields('t', ['id'])->distinct()->execute()->rowCount();
+  $variables['unit_count'] = $unit_count;
+  $variables['tag_count'] = $tag_count;
+ }
+
+
+/**
+ * Override or insert variables into the node template.
+ */
+function whiter_preprocess_node(&$variables) {
+  if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
+    $variables['classes_array'][] = 'node-full';
+  }
+  if ($variables['view_mode'] == 'teaser') {
+    if (isset($variables['content']['field_images']['#items'])) {
+      $count_images = count($variables['content']['field_images']['#items']);
+
+      if ($count_images > 1) {
+        $i = 0;
+        while ($i < $count_images) {
+          unset($variables['content']['field_images'][$i + 1]);
+          $i++;
+        }
+      }
+    }
+  }
+}
+
+function whiter_preprocess_page(&$variables) {
+  if (isset($variables['node'])) {
+
+    // Ref suggestions cuz it's stupid long.
+    $suggests = &$variables['theme_hook_suggestions'];
+
+    // Get path arguments.
+    $args = arg();
+    // Remove first argument of "node".
+    unset($args[0]);
+
+    // Set type.
+    $type = "page__type_{$variables['node']->type}";
+
+    // Bring it all together.
+    $suggests = array_merge(
+      $suggests,
+      array($type),
+      theme_get_suggestions($args, $type)
+    );
+  }
+}
+
+/**
+ * Override or insert variables into the block template.
+ */
+function whiter_preprocess_block(&$variables) {
+  // In the header region visually hide block titles.
+  if ($variables['block']->region == 'header') {
+    $variables['title_attributes_array']['class'][] = 'element-invisible';
+  }
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->region;
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->module;
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->delta;
+
+  // Add block description as template suggestion
+  $block = block_custom_block_get($variables['block']->delta);
+
+  // Transform block description to a valid machine name
+  if (!empty($block['info'])) {
+    setlocale(LC_ALL, 'en_US');
+
+    // required for iconv()
+    $variables['theme_hook_suggestions'][] = 'block__' . str_replace(' ', '_', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $block['info'])));
+  }
+}
+
+/**
+ * Implements theme_menu_tree().
+ */
+function whiter_menu_tree($variables) {
+  return '<ul class="menu clearfix">' . $variables['tree'] . '</ul>';
+}
+
+/**
+ * Implements theme_field__field_type().
+ */
+function whiter_field__taxonomy_term_reference($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
+  }
+
+  // Render the items.
+  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
+  foreach ($variables['items'] as $delta => $item) {
+    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
+  }
+  $output .= '</ul>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
+
+function whiter_theme() {
+  $items = array();
+
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'la') . '/templates',
+    'template' => 'user-login',
+    'preprocess functions' => array(
+      'whiter_preprocess_user_login'
+    ),
+  );
+  $items['user_register_form'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'la') . '/templates',
+    'template' => 'user-register-form',
+    'preprocess functions' => array(
+      'whiter_preprocess_user_register_form'
+    ),
+  );
+  $items['user_pass'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'la') . '/templates',
+    'template' => 'user-pass',
+    'preprocess functions' => array(
+      'whiter_preprocess_user_pass'
+    ),
+  );
+  return $items;
+}
+function whiter_preprocess_user_login(&$vars) {
+  $vars['intro_text'] = t('This is my awesome login form');
+}
+
+function whiter_preprocess_user_register_form(&$vars) {
+  $vars['intro_text'] = t('This is my super awesome reg form');
+}
+
+function whiter_preprocess_user_pass(&$vars) {
+  $vars['intro_text'] = t('This is my super awesome request new password form');
+}
+
+function whiter_menu_tree__main_menu(&$variables) {
+    return '<ul class="nav page-navigation">' . $variables['tree'] . '</ul>';
+}
+
+
+function whiter_menu_link(array $variables) {
+    $element = $variables['element'];
+    if($variables['element']['#theme']== 'menu_link__main_menu'){
+        $attributes = [
+            'attributes' => (array)[
+                'class' => ['nav-link']
+            ]
+        ];
+        $output = l($element['#title'], $element['#href'], $attributes);
+
+        return '<li class="nav-item">' . $output  . "</li>\n";
+    }
+
+
+
+}

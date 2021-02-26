eec10eb9062b407ecef77a6a5c0c19b208f4d166
m33_nft@mail.ru <Jq5Url4zz>
2021-02-26 18:36:25 +0500

    dashboard kns uvd

--- sites/all/themes/la/templates/pages/dashboard.tpl.php
+++ sites/all/themes/la/templates/pages/dashboard.tpl.php
@@ -155,13 +155,13 @@
                         <div class="col-md-12">
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate"
+                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate interactive"
                                             style="float:right; margin-bottom: 10px; " data-href="btn"
-                                            data-browse="212" data-var="door"><i class="fa fa-lock"
+                                            data-browse="312" data-var="door"><i class="fa fa-lock"
                                                                                  aria-hidden="true"></i>
                                     </button>
                                     <a class="default-text" href="/unit/7">
-                                        <h4>КНС УВД (не подключено)</h4>
+                                        <h4>КНС УВД</h4>
                                         <p class="text-muted font-weight-light">ул. Инженерная 5 | SE M241</p>
                                     </a>
                                 </div>
@@ -171,40 +171,34 @@
                             <div class="row">
                                 <div class="col-md-3">
                                     <div class="bar-progress">
-                                        <span class="bar-level bar25" data-href="btn" data-browse="208" data-var="level3"> </span>
-                                        <span class="bar-level bar25" data-href="btn" data-browse="207" data-var="level2"> </span>
-                                        <span class="bar-level bar25" data-href="btn" data-browse="206" data-var="level1"> </span>
-                                        <span class="bar-level bar25" data-href="btn" data-browse="205" data-var="level0"> </span>
+                                        <span class="bar-level bar25 interactive" data-href="btn" data-browse="308" data-var="level3"> </span>
+                                        <span class="bar-level bar25 interactive" data-href="btn" data-browse="307" data-var="level2"> </span>
+                                        <span class="bar-level bar25 interactive" data-href="btn" data-browse="306" data-var="level1"> </span>
+                                        <span class="bar-level bar25 interactive" data-href="btn" data-browse="305" data-var="level0"> </span>
 
                                     </div>
                                 </div>
-
                                 <div class="col-md-9">
-
                                     <p class="small" style="margin-top:20px; margin-bottom: 20px">Насосы в работе:</p>
                                     <div style="margin-left: 10px">
-                                        <p class="btn btn-outline-secondary btn-icon btn-rounded" style="margin-right: 12px;" data-href="btn" data-browse="203" data-error="210"  data-var="engine1.status"><span class="interactive-text" data-browse="219">0</span> A</p>
-                                        <p class="btn btn-outline-secondary btn-icon btn-rounded" style="margin-right: 12px;" data-href="btn" data-browse="204" data-error="211" data-var="engine2.status"><span class="interactive-text" data-browse="220">0</span> A</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" data-browse="303" data-error="310"  data-var="engine1.status"><span class="interactive-text" data-browse="320">0</span> A</p>
+                                        <p class="btn btn-outline-secondary btn-icon btn-rounded interactive" style="margin-right: 12px;" data-href="btn" data-browse="304" data-error="311" data-var="engine2.status"><span class="interactive-text" data-browse="321">0</span> A</p>
                                     </div>
                                     <hr>
-                                    <p style="line-height: 20px">Ошибка уровня: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn" data-browse="209" data-href="btn" data-var="unit.error_level"></button></p>
-                                    <p style="line-height: 20px">Ошибка насоса 1: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn " data-browse="210" data-href="btn"data-var="unit.error_engine"></button></p>
-                                    <p style="line-height: 20px">Ошибка насоса 2: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn " data-browse="211" data-href="btn" data-var="unit.error_engine"></button></p>
+                                    <p style="line-height: 20px">Ошибка уровня: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn interactive" data-browse="309" data-href="btn" data-var="unit.error_level"></button></p>
+                                    <p style="line-height: 20px">Ошибка насоса 1: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn  interactive" data-browse="310" data-href="btn"data-var="unit.error_engine"></button></p>
+                                    <p style="line-height: 20px">Ошибка насоса 2: <button class="btn btn-inverse-light btn-fw btn-sm float-right error-btn  interactive" data-browse="311" data-href="btn" data-var="unit.error_engine"></button></p>
                                     <hr>
-                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate"
+                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate interactive"
                                             style="margin-bottom: 10px; " data-href="btn"
-                                            data-browse="214" data-var="voltage1"><i
+                                            data-browse="314" data-var="voltage1"><i
                                                 class="fa fa-bolt"></i> 1
                                     </button>
-                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate"
+
+                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate interactive"
                                             style="margin-bottom: 10px; " data-href="btn"
-                                            data-browse="215" data-var="voltage2"><i
-                                                class="fa fa-bolt"></i> 2
-                                    </button>
-                                    <button class="btn btn-sm btn-inverse-light btn-fw toolipdate"
-                                            style="margin-bottom: 10px; " data-href="btn"
-                                            data-browse=213" data-var="cold_status"><i
-                                                class="fa fa-snowflake-o" aria-hidden="true"></i> <span class="interactive-text" data-browse="221" style="margin-left: 5px">0</span> ℃</button>
+                                            data-browse=313" data-var="cold_status"><i
+                                                class="fa fa-snowflake-o" aria-hidden="true"></i> <span class="interactive-text" data-browse="319" style="margin-left: 5px">0</span> ℃</button>
 
                                 </div>
 

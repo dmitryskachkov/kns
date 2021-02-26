8382cfcbb80ed181717e32d2f24a08e5ccb49a44
m33_nft@mail.ru <Jq5Url4zz>
2021-02-26 18:46:59 +0500

    dashboard kns uvd

--- sites/all/themes/la/templates/pages/unit--7.tpl.php
+++ sites/all/themes/la/templates/pages/unit--7.tpl.php
@@ -127,7 +127,7 @@ drupal_set_title('КНС УВД');
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-md-8 col-sm-12 col-xs-12">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
                         <a class="default-text" href="/unit/7">
                             <h4>Дренаж</h4>
                             <p class="text-muted font-weight-light">Уровень в дренажном резервуаре</p>
@@ -147,7 +147,7 @@ drupal_set_title('КНС УВД');
 
 
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-12">
                         <h4>Насос</h4>
                         <p class="text-muted font-weight-light">Сигналы насосов</p>
                         <div style="margin-left: 10px">

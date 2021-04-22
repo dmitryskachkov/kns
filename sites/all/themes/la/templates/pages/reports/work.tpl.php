de861d2b5ee53aa11bc1b986a20b527c2d44b289
m33_nft@mail.ru <Jq5Url4zz>
2021-04-22 14:42:01 +0500

    Entity refenece

--- sites/all/themes/la/templates/pages/reports/work.tpl.php
+++ sites/all/themes/la/templates/pages/reports/work.tpl.php
@@ -8,26 +8,31 @@
                     </div>
                     <?php if ($units): ?>
                         <?php foreach($units as $unit): ?>
-                            <h5><?= $unit->title ?></h5>
-                            <p>Адрес: <?= $unit->description; ?></p>
-                            <hr>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <td>№</td>
-                                        <td>Название агрегата</td>
-                                        <td>Наработка</td>
-                                        <td>Производительность</td>
-                                        <td>Объём стоков</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="4">Итого</td>
-                                        <td>0.0</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <h5><?= $unit->title ?></h5>
+                                    <p>Адрес: <?= $unit->description; ?></p>
+                                    <hr>
+                                    <table>
+                                        <thead>
+                                        <tr>
+                                            <td>№</td>
+                                            <td>Название агрегата</td>
+                                            <td>Наработка</td>
+                                            <td>Производительность</td>
+                                            <td>Объём стоков</td>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td colspan="4">Итого</td>
+                                            <td>0.0</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </div>

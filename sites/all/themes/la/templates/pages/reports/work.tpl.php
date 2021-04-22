633e30b60053046830551c0570e292f6af75601e
m33_nft@mail.ru <Jq5Url4zz>
2021-04-22 14:36:30 +0500

    Entity refenece

--- sites/all/themes/la/templates/pages/reports/work.tpl.php
+++ sites/all/themes/la/templates/pages/reports/work.tpl.php
@@ -3,12 +3,33 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-lg-8">
-                        <h2>Отчёт о наработке всех насосных агрегатов</h2>
+                    <div class="float-right">
+                        <?php print render($period_switch_form) ; ?>
                     </div>
-                    <div class="col-lg-4">
-                        <?php print render($unit_switch_form) ; ?>
-                    </div>
+                    <?php if ($units): ?>
+                        <?php foreach($units as $unit): ?>
+                            <h5><?= $unit->title ?></h5>
+                            <p>Адрес: <?= $unit->description; ?></p>
+                            <hr>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td>№</td>
+                                        <td>Название агрегата</td>
+                                        <td>Наработка</td>
+                                        <td>Производительность</td>
+                                        <td>Объём стоков</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="4">Итого</td>
+                                        <td>0.0</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
                 </div>
 
             </div>

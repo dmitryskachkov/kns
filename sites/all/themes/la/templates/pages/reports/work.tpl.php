35c9cd247c6facbc7f40cf03ccc780ca28bcf431
Dmitry <m33_nft@mail.ru>
2021-04-23 00:07:21 +0500

    update

--- sites/all/themes/la/templates/pages/reports/work.tpl.php
+++ sites/all/themes/la/templates/pages/reports/work.tpl.php
@@ -3,16 +3,20 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <?php print render($period_switch_form) ; ?>
+                        <div>
+                            <div style="float: right">
+                              <?php print render($period_switch_form) ; ?>
+                            </div>
                         </div>
-                    </div>
+
+
                     <?php if ($units): ?>
+
+
                         <?php foreach($units as $unit): ?>
-                            <div class="row">
+
                                 <div class="col-lg-12">
-                                    <h5><?= $unit->title ?></h5>
+                                    <h4><?= $unit->title ?></h4>
                                     <p>Адрес: <?= $unit->description; ?></p>
                                     <hr>
                                     <table class="table table-striped">
@@ -26,18 +30,31 @@
                                         </tr>
                                         </thead>
                                         <tbody>
+                                        <?php if (isset($unit->engines)) : ?>
+                                            <?php foreach ($unit->engines as $n => $engine): ?>
+                                                <tr>
+                                                    <td><?= $n ?></td>
+                                                    <td><?= $engine->title; ?></td>
+                                                    <td><?= core_perfomance_engine_stat($engine->field_tag_id['und'][0]['value'], $period) ?></td>
+                                                    <td><?= $engine->field_performance_q['und'][0]['value'] ?> м3/час</td>
+                                                    <td><?= core_perfomance_engine_stat($engine->field_tag_id['und'][0]['value'], $period) * $engine->field_performance_q['und'][0]['value'] ?></td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        <?php endif; ?>
                                         <tr>
-                                            <td colspan="4">Итого</td>
+                                            <td colspan="4" style="text-align: right; font-weight: bold">Итого</td>
                                             <td>0.0</td>
                                         </tr>
                                         </tbody>
                                     </table>
+                                    <hr style="margin-bottom: 30px">
                                 </div>
-                            </div>
+
 
                         <?php endforeach; ?>
+                        </div>
                     <?php endif; ?>
-                </div>
+
 
             </div>
         </div>
